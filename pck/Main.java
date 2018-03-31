dde02698d57275a4a7d21db750dc62aaade0888a
Publicemail <toropumihai@gmail.com>
2018-03-31 14:49:14 +0300

    working version
    http://www.mpj-express.org/docs/javadocs/mpi/Comm.html

--- pck/Main.java
+++ pck/Main.java
@@ -3,63 +3,60 @@ package pck;
 
 import mpi.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
 
     public static void main(String[] args) {
-        List<Double> myList = new ArrayList<Double>();
-        myList.add(5.0);
-        myList.add(6.1);
-        myList.add(7.2);
-        myList.add(8.3);
-        myList.add(9.5);
-        myList.add(10.6);
-        myList.add(11.2);
-        myList.add(12.9);
-
 
         MPI.Init(args);
-        Double[] sum = new Double[1];
-        Double totalSum = 0.0;
+        int[] sum = new int[1];
+        int totalSum = 0;
         int stop = 0;
+        int j;
         int rank = MPI.COMM_WORLD.Rank();
-        System.out.println("THIS IS THE RANK " + rank);
         int size = MPI.COMM_WORLD.Size();
-        System.out.println("THIS IS THE SIZE: 4 = " + size);
-        int sizePerProcess = myList.size() / 4;
-        int lowerBound = rank * sizePerProcess;
-        int upperBound = lowerBound + sizePerProcess;
+        int vectorSize = 8;
+        int sizePerProcess = vectorSize / size;
+        int[] buffer = new int[sizePerProcess];
+        int[] vector = new int[vectorSize];
 
         if (0 == rank) {
+
+            for (int i = 0; i < vectorSize; i++) {
+                vector[i] = 5 + i;
+            }
             if (0 == stop) {
                 for (int k = 0; k < sizePerProcess; k++) {
-                    totalSum += myList.get(k);
+                    totalSum += vector[k];
                 }
+                stop = 1;
+                System.out.println("CPU " + rank + "a adunat " + totalSum);
+            }
+
+            for (int i = 1; i < size; i++) {
+                j = 0;
+                for (int k = i * sizePerProcess; k < i * sizePerProcess + sizePerProcess; k++) {
+                    buffer[j] = vector[k];
+                    j++;
+                }
+                MPI.COMM_WORLD.Send(buffer, 0, sizePerProcess, MPI.INT, i, 0);
             }
         }
 
-        if (rank != 0) {
-            lowerBound = rank * sizePerProcess;
-            upperBound = lowerBound + sizePerProcess;
-            System.out.println("LowerBound is " + lowerBound);
-            System.out.println("upperBound is " + upperBound);
-            for (int i = lowerBound; i < upperBound; i++) {
-
-                sum[0] =+ myList.get(i);
+        if (0 != rank) {
+            MPI.COMM_WORLD.Recv(buffer, 0, sizePerProcess, MPI.INT, 0, 0);
+            for (int i = 0; i < sizePerProcess; i++) {
+                sum[0] += buffer[i];
             }
             System.out.println("CPU " + rank + "a adunat " + sum[0]);
-            MPI.COMM_WORLD.Send(sum[0], 0, 1, MPI., 0, 0);
+            MPI.COMM_WORLD.Send(sum, 0, 1, MPI.INT, 0, 0);
+        } else {
+            for (int i = 1; i < 4; i++) {
+                MPI.COMM_WORLD.Recv(sum, 0, 1, MPI.INT, i, 0);
+                totalSum += sum[0];
+            }
+            System.out.println("Suma totala este " + totalSum);
         }
 
-        if (0 == rank) {
-            System.out.println("Suma totala este " + totalSum);
-            for (int i = 1; i < 4; i++) {
-                MPI.COMM_WORLD.Recv(sum[0], 0, 1, MPI.DOUBLE, i, 0);
-                totalSum += (Double)sum[0];
-            }
-        }
         MPI.Finalize();
     }
 }
